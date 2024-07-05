bec5b4149ca7b46261558682a3edb9301351bf98
Gábor Szekeres <mail.szekg@gmail.com>
2024-07-05 17:02:03 +0200

    Practice

--- script2.js
+++ script2.js
@@ -1,9 +1,35 @@
-console.log("sziasztok")
-
 async function fetchData() {
     const fetchResult = await fetch("https://swapi.dev/api/people/")
     const data = await fetchResult.json()
-    console.log(data.results)
+    const characters = data.results //characters = array [10]
+    //console.log(characters)
+    
+const rootElement = document.querySelector("#root")
+let charactersHtml = ""
+
+/* for (let i = 0; i < characters.length; i++) {
+    console.log(characters[i])
+
+    charactersHtml += `
+        <div class="character">
+            <p class="name">${characters[i].name} </p>
+            <p class="height">${characters[i].height} </p>
+            <p class="weight">${characters[i].mass} </p>
+        </div>
+    `
+} */
+
+characters.forEach(character => charactersHtml += `
+        <div class="character>
+            <p class="name"> ${character.name}</p>
+            <p class="height"> ${character.height} cm</p>
+            <p class="weight"> ${character.weight} kg</p>
+        </div>
+    `
+)
+
+rootElement.insertAdjacentHTML("beforeend", charactersHtml)
+
 }
 
 fetchData()
