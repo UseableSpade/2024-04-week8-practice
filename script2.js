71a28e7db0ec076400eecc4caeadf13308175e23
Gábor Szekeres <mail.szekg@gmail.com>
2024-07-08 18:45:54 +0200

    Added second page

--- script2.js
+++ script2.js
@@ -1,16 +1,16 @@
-const characterComponent = (name, height, weight) => `
+const characterComponent = (name, height, mass) => `
     <div class="character">
         <h2>Character:</h2>
         <p class="name">${name}</p>
         <p class="height">${height} cm</p>
-        <p class="weight">${weight} kg</p>
+        <p class="weight">${mass} kg</p>
     </div>
 `
 
 const charactersComponent = (charactersData) => `
     <div class="characters">
         ${charactersData
-            .map(characterData => characterComponent(characterData.name, characterData.height, characterData.weight))
+            .map(characterData => characterComponent(characterData.name, characterData.height, characterData.mass))
             .join(" ")
         }
     </div>
@@ -22,14 +22,21 @@ async function fetchData() {
     const characters = data.results
     
     const rootElement = document.querySelector("#root")
-
     rootElement.insertAdjacentHTML("beforeend", charactersComponent(characters))
     rootElement.insertAdjacentHTML("beforeend", `<button class="fetch">load more...</button>`)
-
+    
     const fetchButtonElement = document.querySelector("button.fetch")
-    fetchButtonElement.addEventListener("click", () => {
+    fetchButtonElement.addEventListener("click", async () => {
         console.log("fetch next page")
         console.log(data.next)
+        
+        const newFetchResult = await fetch(data.next)
+        console.log(newFetchResult)
+        const newData = await newFetchResult.json()
+        console.log(newData)
+        const newCharacters = newData.results
+        
+        rootElement.insertAdjacentHTML("beforeend", charactersComponent(newCharacters))
     })
 }
 
